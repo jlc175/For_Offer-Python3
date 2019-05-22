86d525ea25a310313d22f9ecf4484fc2ad6f3bc6
Jing Licheng <jlc@mail.ustc.edu.cn>
2019-05-22 17:46:47 +0800

    Completed

---
+++ 数据结构/数组/4.二维数组中的查找/FindInPartiallySortedMatrix.py
@@ -0,0 +1,26 @@
+def find(matrix, rows, columns, number):
+    if matrix != None and rows > 0 and columns > 0:
+        # 从右上角开始
+        row = 0
+        column = columns - 1
+        while row < rows and column >= 0:
+            if matrix[row][column] == number:
+                return True
+            elif matrix[row][column] > number:
+                column -= 1
+            else:
+                row += 1
+        return False
+    return False
+
+
+if __name__ == "__main__":
+    case1 = [[1, 2, 8, 9],
+             [2, 4, 9, 12],
+             [4, 7,10, 13],
+             [6, 8, 11, 15]]
+    case2 = []
+
+    print('包含该数字', find(case1, 4, 4, 7))
+    print('不含该数字', find(case1, 4, 4, 3))
+    print('矩阵为空', find(case2, 0, 0, 7))
