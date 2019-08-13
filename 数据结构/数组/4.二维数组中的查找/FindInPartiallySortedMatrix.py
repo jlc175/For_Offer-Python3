c46ba385cc53376895b228117896b741eb245efe
jinglicheng <jlc@mail.ustc.edu.cn>
2019-08-13 16:50:52 +0800

    优化编码规范

--- 数据结构/数组/4.二维数组中的查找/FindInPartiallySortedMatrix.py
+++ 数据结构/数组/4.二维数组中的查找/FindInPartiallySortedMatrix.py
@@ -1,5 +1,5 @@
 def find(matrix, rows, columns, number):
-    if matrix != None and rows > 0 and columns > 0:
+    if matrix is not None and rows > 0 and columns > 0:
         # 从右上角开始
         row = 0
         column = columns - 1
@@ -17,7 +17,7 @@ def find(matrix, rows, columns, number):
 if __name__ == "__main__":
     case1 = [[1, 2, 8, 9],
              [2, 4, 9, 12],
-             [4, 7,10, 13],
+             [4, 7, 10, 13],
              [6, 8, 11, 15]]
     case2 = []
 
